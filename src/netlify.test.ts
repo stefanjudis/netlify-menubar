85571e5a9521751edaec19854181b74429acf390
stefan judis <stefanjudis@gmail.com>
2019-01-16 18:47:20 +0000

    Add more tests for netlify module

--- src/netlify.test.ts
+++ src/netlify.test.ts
@@ -1,7 +1,12 @@
-import fetch from 'node-fetch';
+import * as fetch from 'node-fetch';
 import Netlify from './netlify';
 
+// TODO place this in global config somehwere
+// tslint:disable-next-line
+console.log = () => {};
+
 jest.mock('node-fetch');
+const mFetch = fetch.default as jest.Mock<typeof fetch.default>;
 
 describe('netlify api client', () => {
   const apiToken = 'awesomeToken';
@@ -11,5 +16,32 @@ describe('netlify api client', () => {
 
   test('accessToken is stored in api client', () => {
     expect(apiClient.accessToken).toBe(apiToken);
+  });
+
+  test('authorization header is properly set', async () => {
+    mFetch.mockReturnValue(
+      new Promise(res1 =>
+        res1({
+          json: () => new Promise(res => res())
+        })
+      )
+    );
+    apiClient.fetch('/foo');
+    expect(mFetch.mock.calls[0][0].endsWith('/foo')).toBeTruthy();
+    expect(mFetch.mock.calls[0][1].headers.authorization).toBe(
+      `Bearer ${apiToken}`
+    );
+  });
+
+  test("throws 'NOT_AUTHORIZED' error in case of 401", async () => {
+    mFetch.mockReturnValue(
+      new Promise(res =>
+        res({
+          foo: 'bar',
+          status: 401
+        })
+      )
+    );
+    await expect(apiClient.fetch('/foo')).rejects.toThrow('NOT_AUTHORIZED');
   });
 });
