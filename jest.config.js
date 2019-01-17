8e31087f1c9109ce90ee5ba809b04cac01eb96a4
stefan judis <stefanjudis@gmail.com>
2019-01-17 15:16:46 +0000

    Define that tests run only against ts files

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  roots: ['<rootDir>/src'],
   collectCoverage: true,
   collectCoverageFrom: ['src/*.ts', '!**/node_modules/**']
 };
