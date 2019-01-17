23f5656a102494c1a08a5f72598cf28f759e988d
stefan judis <stefanjudis@gmail.com>
2019-01-17 22:17:03 +0000

    Use mockResolvedValue instead of mockReturnValue

--- src/netlify.test.ts
+++ src/netlify.test.ts
@@ -7,13 +7,16 @@ console.log = () => {};
 
 jest.mock('node-fetch');
 
-const getFetchPromise = (json: {} = {}, response: {} = {}): Promise<any> => {
-  return new Promise(res1 =>
-    res1({
-      ...(response && response),
-      json: () => new Promise(res2 => res2(json))
-    })
-  );
+interface FetchResponse {
+  response?: number;
+  json: () => {};
+}
+
+const getFetchPromise = (json: {} = {}, response: {} = {}): FetchResponse => {
+  return {
+    ...(response && response),
+    json: () => new Promise(res => res(json))
+  };
 };
 
 describe('netlify api client', () => {
@@ -32,7 +35,7 @@ describe('netlify api client', () => {
     });
 
     test('sets authorization header properly', async () => {
-      mFetch.mockReturnValue(getFetchPromise());
+      mFetch.mockResolvedValue(getFetchPromise());
       await apiClient.fetch('/foo');
       expect(mFetch.mock.calls[0][0].endsWith('/foo')).toBeTruthy();
       expect(mFetch.mock.calls[0][1].headers.authorization).toBe(
@@ -41,7 +44,7 @@ describe('netlify api client', () => {
     });
 
     test("rejects 'NOT_AUTHORIZED' error in case of 401", async () => {
-      mFetch.mockReturnValue(
+      mFetch.mockResolvedValue(
         getFetchPromise(
           {},
           {
@@ -54,7 +57,7 @@ describe('netlify api client', () => {
 
     test('returns the correct response', async () => {
       const response = { foo: 'bar' };
-      mFetch.mockReturnValue(getFetchPromise(response));
+      mFetch.mockResolvedValue(getFetchPromise(response));
 
       const result = await apiClient.fetch('/foo');
       expect(result).toBe(response);
@@ -63,7 +66,7 @@ describe('netlify api client', () => {
 
   describe(':getCurrentUser', () => {
     test('calls the correct URL', async () => {
-      mFetch.mockReturnValue(getFetchPromise({}));
+      mFetch.mockResolvedValue(getFetchPromise({}));
       await apiClient.getCurrentUser();
       expect(mFetch.mock.calls[0][0]).toBe(`${API_URL}/user`);
     });
@@ -71,7 +74,7 @@ describe('netlify api client', () => {
 
   describe(':getSites', () => {
     test('calls the correct URL', async () => {
-      mFetch.mockReturnValue(getFetchPromise({}));
+      mFetch.mockResolvedValue(getFetchPromise({}));
       await apiClient.getSites();
       expect(mFetch.mock.calls[0][0]).toBe(`${API_URL}/sites`);
     });
@@ -80,7 +83,7 @@ describe('netlify api client', () => {
   describe(':getSiteDeploys', () => {
     test('calls the correct URL', async () => {
       const siteId = '123456789';
-      mFetch.mockReturnValue(getFetchPromise({}));
+      mFetch.mockResolvedValue(getFetchPromise({}));
       await apiClient.getSiteDeploys(siteId);
       expect(mFetch.mock.calls[0][0]).toBe(
         `${API_URL}/sites/${siteId}/deploys`
@@ -91,7 +94,7 @@ describe('netlify api client', () => {
   describe(':createSiteBuild', () => {
     test('calls the correct URL with the correct HTTP method', async () => {
       const siteId = '123456789';
-      mFetch.mockReturnValue(getFetchPromise({}));
+      mFetch.mockResolvedValue(getFetchPromise({}));
       await apiClient.createSiteBuild(siteId);
       expect(mFetch.mock.calls[0][0]).toBe(`${API_URL}/sites/${siteId}/builds`);
       expect(mFetch.mock.calls[0][1].method).toBe('POST');
@@ -100,7 +103,7 @@ describe('netlify api client', () => {
 
   describe(':authorize', () => {
     test('calls the API once to check validity of the token', async () => {
-      mFetch.mockReturnValue(getFetchPromise({}));
+      mFetch.mockResolvedValue(getFetchPromise({}));
       await apiClient.authorize('clientId');
       expect(mFetch.mock.calls.length).toBe(1);
     });
