f324d2a407ddeec9fcdd546a59300e88d648be38
Adam Watters <adamwatters11@gmail.com>
2019-02-22 22:44:02 -0500

    exporting mock methods from mockElectron

--- mocks/electronMock.ts
+++ mocks/electronMock.ts
@@ -1,6 +1,10 @@
 export const mockOn = jest.fn();
 export const mockShow = jest.fn();
+export const inMockConstructor = jest.fn();
 export class Notification {
   public on = mockOn;
   public show = mockShow;
+  constructor() {
+    inMockConstructor();
+  }
 }
--- src/notify.test.ts
+++ src/notify.test.ts
@@ -2,20 +2,22 @@ import * as electron from 'electron'; // tslint:disable-line no-implicit-depende
 import settings from 'electron-settings';
 import notify from './notify';
 jest.mock('electron-settings');
+const mockElectron = electron as any;
+const mockGet = settings.get as jest.Mock; // otherwise ts doesn't think get has :mockImplementation
 
 describe('notify function', () => {
-  test('does not create a notification if showNotifications is set to false', () => {
-    const mockGet = settings.get as jest.Mock; // otherwise ts doesn't think get has :mockImplementation
+  test('if showNotifications setting is false, does not create a Notification ', () => {
     mockGet.mockImplementation(() => false); // getting any setting will return false
     notify({ title: 'test title', body: 'test body' }, jest.fn());
     expect(mockGet).toHaveBeenCalledWith('showNotifications');
     expect(mockGet.mock.results[0].value).toEqual(false);
+    expect(mockElectron.inMockConstructor).not.toHaveBeenCalled();
   });
-  test('if showNotifications is set to true, it creates a notification then calls notification.show', () => {
-    const mockGet = settings.get as jest.Mock; // otherwise ts doesn't think get has :mockImplementation
-    const mockElectron = electron as any;
+  test('if showNotifications setting is true, it creates a Notification, registers callbacks, calls notifation.show()', () => {
     mockGet.mockImplementation(() => true);
     notify({ title: 'test title', body: 'test body' }, jest.fn());
+    expect(mockElectron.inMockConstructor).toHaveBeenCalled();
+    expect(mockElectron.mockOn).toHaveBeenCalledTimes(2);
     expect(mockElectron.mockShow).toHaveBeenCalled();
   });
 });
